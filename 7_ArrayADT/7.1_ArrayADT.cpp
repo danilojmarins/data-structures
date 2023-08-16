662dd684be098a3bc0f0b1c87185a64c14a182c3
danilojmarins <danilojmarins@hotmail.com>
2023-08-16 13:05:23 -0300

    Array Methods: Min(), Max(), Sum(), Avg(), Reverse().

--- 7_ArrayADT/7.1_ArrayADT.cpp
+++ 7_ArrayADT/7.1_ArrayADT.cpp
@@ -50,9 +50,14 @@ void Display(struct Array arr)
     }
 }
 
-int Get(struct Array *arr, int index)
+int Get(struct Array arr, int index)
 {
-    return arr->A[index];
+    if (index >= 0 && index < arr.length)
+    {
+        return arr.A[index];
+    }
+
+    return -1;
 }
 
 void Set(struct Array *arr, int index, int value)
@@ -61,6 +66,53 @@ void Set(struct Array *arr, int index, int value)
     {
         arr->A[index] = value;
     }
+}
+
+int Max(struct Array arr)
+{
+    int max = arr.A[0];
+
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] > max)
+        {
+            max = arr.A[i];
+        }
+    }
+
+    return max;
+}
+
+int Min(struct Array arr)
+{
+    int min = arr.A[0];
+
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (arr.A[i] < min)
+        {
+            min = arr.A[i];
+        }
+    }
+
+    return min;
+}
+
+int Sum(struct Array arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < arr.length; i++)
+    {
+        sum = sum + arr.A[i];
+    }
+
+    return sum;
+}
+
+float Average(struct Array arr)
+{
+    return Sum(arr) / arr.length;
 }
 
 void Append(struct Array *arr, int value)
@@ -153,6 +205,14 @@ int BinarySearch(struct Array *arr, int key)
     return -1;
 }
 
+void Reverse(struct Array *arr)
+{
+    for (int i = 0, j = arr->length - 1; i < j; i++, j--)
+    {
+        Swap(arr, i, j);
+    }
+}
+
 int main()
 {
     struct Array arr;
@@ -169,7 +229,13 @@ int main()
     }
 
     //printf("\n%d \n", LinearSearch(&arr, 3));
-    printf("\n%d \n", BinarySearch(&arr, 3));
+    //printf("\n%d \n", BinarySearch(&arr, 3));
+
+    //printf("\n%d \n", Max(arr));
+
+    Display(arr);
+
+    Reverse(&arr);
 
     Display(arr);
 
